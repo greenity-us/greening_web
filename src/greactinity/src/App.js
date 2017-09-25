f7a2d87ada644a80a10bbbbf4f0b63ceea4a3a9c
Yatit <thakkery@gmail.com>
2017-09-24 21:43:34 -0700

    Bootstrapped React App with Basic Map
    - Converted frontend to react app
    - Added basic map with search
    - Still working on adding data, more copy, and better search functionality for the map

---
+++ src/greactinity/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { MapWithASearchBox } from './map';
+
+class App extends Component {
+  render() {
+    return (
+      <MapWithASearchBox />
+    );
+  }
+}
+
+export default App;
